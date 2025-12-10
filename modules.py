e86bd15c8529158ba0c9e7d8e8a034f55e186d88
adriafonss <230268889 +230268889+adrianafonss306@users.noreply.github.com>
2025-12-10 16:36:17 -0500

    update

--- modules.py
+++ modules.py
@@ -1,22 +1,26 @@
-def mipyme_name():
+def mipyme_name(): #nombre de todas las mipymes
     import json
     with open ("./proyecto_icd.json","r",encoding="utf-8") as file:
         data=json.load(file)
     
         mipymes = data["mipymes"]
         for i in mipymes:
-            return i["store"]["name"]        
+            print(i["store"]["name"])
 
+mipyme_name()
 
-def mipyme_amount():
+def mipyme_amount(): #cantidad de mipymes
     import json
     with open ("./proyecto_icd.json","r",encoding="utf-8") as file:
         data=json.load(file)
     
         mipymes = data["mipymes"]
-        return len(mipymes)   
+        return len(mipymes)
+    
+print(mipyme_amount())
 
-def name_products():
+
+def name_products(): #nombre de productos analizados 
     import json
     with open ("./proyecto_icd.json","r",encoding="utf-8") as file:
         datos=json.load(file)
@@ -24,36 +28,30 @@ def name_products():
         empty = []
         mipymes = datos["mipymes"]
         for i in range(len(mipymes)):
-            all_products= mipymes[i]["store"]["products"]
-            for j in range(len(all_products)):
-                if all_products[j]["name"] not in empty:            
-                    empty.append(all_products[j]["name"]) 
-                    return", ".join(empty) 
+            products_food=mipymes[i]["store"]["products"][0]["groceries"]         
+            for j in products_food:
+                if j not in empty:
+                    empty.append(j)
+            products_bath=mipymes[i]["store"]["products"][0]["hygiene"]
+            for k in products_bath:  
+                if k not in empty:
+                    empty.append(k)
+    print(empty)
+    
+name_products()
 
-
-def name_products_amount():
+def name_products_amount(): #nombre del producto y cantidad del mismo en una mipyme
     import json
     with open ("./proyecto_icd.json","r",encoding="utf-8") as file:
         data=json.load(file)
-    
-        mipymes = data["mipymes"]
-        for i in mipymes:
-            prod_amount = len(i["store"]["products"])
-            return i["store"]["name"], prod_amount
         
-
-
-def average_prices():#son todos los productos sin categorizar
-    import json
-    with open("./proyecto_icd.json","r", encoding = "utf-8") as file:
-        data = json.load(file)
-    
-        list_prices = []
-        mipymes = data["mipymes"]
+        mipymes = data["mipymes"]    
         for i in range(len(mipymes)):
-            for j in range(len(mipymes[i]["store"]["products"])):
-                if mipymes[i]["store"]["products"][j]["price"] not in list_prices:                
-                    list_prices.append(mipymes[i]["store"]["products"][j]["price"])
-                    
-                average = sum(list_prices)/len(list_prices)
-        return average
+            products = mipymes[i]["store"]["products"]
+            for j in products:
+                prod_food=j["groceries"]
+            for k,l in prod_food.items():
+                print(k,len(l))
+            print("--------") 
+            
+name_products_amount()
